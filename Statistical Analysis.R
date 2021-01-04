3fb8b9547783435b7cc7b3c0d5911a0e1d765c57
EduardFabregat <45469244+EduardFabregat@users.noreply.github.com>
2021-01-04 14:58:30 -0500

    Update Statistical Analysis.R

--- Statistical Analysis.R
+++ Statistical Analysis.R
@@ -1,75 +1,83 @@
 ######################################################
 ########## Statistical Analysis
 
-ref <- Variables_Refugiats_2_intent
+ref <- readxl::read_excel("1 new a analysis after Rej/Variables Refugiats 2 intent.xlsx")
+
+ref$Country
+all_frames$Group.1
+
+ref[29,1] <- "United States"
+ref[27,1] <- "United Kingdom"
+all_frames[15,1] <- "Uganda"
+colnames(all_frames)[1] <- "Country"
+
+ref <- ref[which(ref$Country %in% all_frames$Country),]
+
+ref <- ref[c(1, 5:ncol(ref))]
+
+ref <- cbind(all_frames[all_frames$Country != "Singapore", ], ref)
+
+ref <- ref[, -5]
 
 colnames(ref)
 
+ref$conflict <- as.numeric(ref$conflict)
+
+ref$Distance_Country_1.km.<-as.numeric(ref$Distance_Country_1.km.)
+ref$Distance_Country_2.km.<-as.numeric(ref$Distance_Country_2.km.)
 ref<-ref[ref$Country!="Singapore",]
-
-
-attach(ref)
-
-#Moral Universal
-myvars <- c(2,5,6,7,8,9,10,11,12,13,14,15,16)
-
-colnames(ref)[myvars]
-
-apa.cor.table(ref[,myvars], filename="ex.CorTable1.m_u.doc")
-
-m_u <- lm(solutions~Distance_Country_1.km +Political_Instability, ref)
-summary(m_u)
-vif(m_u)
-bptest(m_u)
-
-plot(m_u)
-
-apa.reg.table(m_u, filename = "regtable.m_u")
-
-p1<-ggplot(ref,aes(x=ref$solutions))+geom_point(aes(y=ref$Distance_Country_1.km))+
-  xlab("Moral-Universal Frame Salience")+ylab("Distance to country of Refugees")+theme_bw()
-p2<-ggplot(ref,aes(x=ref$solutions))+geom_point(aes(y=ref$Political_Instability))+
-  xlab("Moral-Universal Frame Salience")+ylab("Political Instability")+theme_bw()
-
-
-require(gridExtra)
-grid.arrange(p1, p2, ncol=2)
-
-#Conflict
-
-myvars <- c(3,5,6,7,8,9,10,11,12,13,14,15,16)
-
-colnames(ref)[myvars]
-
-apa.cor.table(ref[,myvars], filename="ex.CorTable1.Conflict.doc")
+ref$Government<-as.factor(ref$Government)
+ref2<-select(ref,-c(Origin_Refugees,Distance_to_Myanmar,Distance_to_SouthSudan,Distance_to_Syria,Population_Below_Poverty_Line))
 
 attach(ref)
 
-conflict <- lm(conflict~Distance_Country_2.km +Political_Instability,ref)
-summary(conflict)
-vif(conflict)
-bptest(conflict)
 
-plot(conflict)
+hist(moral)
+hist(conflict)
+hist(politics)
 
-apa.reg.table(conflict, filename = "regtable.conflict")
+#Conflit
+myvars <- c(2,5:16)
 
+colnames(ref)[myvars]
 
-######################
+apa.cor.table(ref[,myvars])
+apa.cor.table(ref[,myvars], filename="Conflict.Corr.doc")
 
-p3<-ggplot(ref,aes(x=ref$conflict))+geom_point(aes(y=ref$Distance_Country_2.km))+
-  xlab("Conflict Frame Salience")+ylab("Distance to country of Refugees")+theme_bw()
-p4<-ggplot(ref,aes(x=ref$conflict))+geom_point(aes(y=ref$Political_Instability))+
-  xlab("Conflict Frame Salience")+ylab("Political Instability")+theme_bw()
+#Moral
+myvars <- c(3,5:16)
 
+colnames(ref)[myvars]
 
-require(gridExtra)
-grid.arrange(p1, p2, p3, p4, ncol=2)
+apa.cor.table(ref[,myvars])
+apa.cor.table(ref[,myvars], filename="Moral.Corr.doc")
 
 #Politics
-
-myvars <- c(4,5,6,7,8,9,10,11,12,13,14,15,16)
+myvars <- c(4,5:16)
 
 colnames(ref)[myvars]
 
-apa.cor.table(ref[,myvars], filename="ex.CorTable1.Politics.doc")
+apa.cor.table(ref[,myvars])
+apa.cor.table(ref[,myvars], filename="Politics.Corr.doc")
+
+moral_press <- lm(moral~Press_Freedom_Index, ref)
+moral_dist <- lm(moral~`Distance_Country_1.km`, ref)
+
+apa.reg.table(moral_press, filename = "moral_press.doc")
+apa.reg.table(moral_dist, filename = "moral_dist.doc")
+
+conflict_gdp <- lm(conflict~`GDP(percapita)`, ref)
+conflict_peace <- lm(conflict~Peace_Index, ref)
+
+apa.reg.table(conflict_gdp, filename = "conflict_gdp.doc")
+apa.reg.table(conflict_peace, filename = "conflict_peace.doc")
+
+
+pol_dist <- lm(politics~Distance_Country_1.km, ref)
+pol_gdp <- lm(politics~`GDP(percapita)`, ref)
+pol_press <- lm(politics~Press_Freedom_Index, ref)
+
+apa.reg.table(pol_dist, filename = "pol_dist.doc")
+apa.reg.table(pol_gdp, filename = "pol_gdp.doc")
+apa.reg.table(pol_press, filename = "pol_press.doc")
+
